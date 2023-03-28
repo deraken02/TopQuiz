a18d26bbfb97863565814239a5cabc92dc3b85ed
Louis Delacroix <louisdelacroix3@gmail.com>
2023-03-28 20:01:07 +0200

    Deux premieres leçons

---
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.topquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingTextView = findViewById(R.id.main_textview_greeting);
+        mNameEditText = findViewById(R.id.main_edittext_name);
+        mPlayButton = findViewById(R.id.main_button_play);
+        mPlayButton.setEnabled(false);
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
